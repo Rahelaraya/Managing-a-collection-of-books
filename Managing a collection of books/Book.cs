710de89ef5ecb04b1802ef157de0431147f6f577
Rahelaraya <rahelaraya26@gmail.com>
2024-10-10 19:25:26 +0200

    Add project files.

---
+++ Managing a collection of books/Book.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Managing_a_collection_of_books
+{
+    public class Book
+    {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public int YearPublished { get; set; }
+
+
+            public Book(string title, string author, int yearPublished)
+            {
+                Title = title;
+                Author = author;
+                YearPublished = yearPublished;
+            }
+    }
+    
+}
---
+++ Managing a collection of books/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Managing_a_collection_of_books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            static void Main(string[] args)
+            {
+                Book bookToAdd = new Book("", "", 1);
+
+                List<Book> BookList = new List<Book>();
+
+
+                Console.WriteLine("pleas write the books Title");
+                bookToAdd.Title = Console.ReadLine()!;
+
+                Console.WriteLine("pleas write  the books Author");
+                bookToAdd.Author = Console.ReadLine()!;
+
+                Console.WriteLine("pleas write the books YearPublished ");
+                bookToAdd.YearPublished = Convert.ToInt32(Console.ReadLine()!);
+
+                BookList.Add(bookToAdd);
+                foreach (Book book in BookList)
+                {
+                    Console.WriteLine(book.Title);
+
+                }
+
+
+
+                Book book1 = new Book("See", "Tom", 1);
+                Book book2 = new Book("Car", "Lee", 2);
+                Book book3 = new Book("Pia", "Jane", 3);
+
+                BookList.Add(book1);
+                BookList.Add(book2);
+                BookList.Add(book3);
+
+                Console.Write("\nEnter title to search: ");
+                string searchTitle = Console.ReadLine();
+                SearchBookByTitle(searchTitle, BookList);
+                Console.ReadKey();
+                static void SearchBookByTitle(string titleToSearch, List<Book> BookList)
+                {
+                    var foundBook = BookList.Find(b => b.Title.ToLower() == titleToSearch.ToLower());
+                    if (foundBook != null)
+                    {
+                        Console.WriteLine($"Found book: Title: {foundBook.Title}, Author: {foundBook.Author}, Year Published: {foundBook.YearPublished}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Book not found.");
+                    }
+                }
+            }
+        }
+    }
+ }
+
